c59a9662f47e8ef8dd8040bdefb848ca1c0e9e21
Xander Harrison <xander.harrison77@gmail.com>
2021-05-22 14:36:37 -0400

    Cleaned up the code
    - Fixed issue where players aren't being added when they connect.
    
    - Fixed players not being removed when they leave.

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,22 +3,28 @@ import { PlayerManager } from "./managers/players";
 import { Events } from "../shared/enums/events";
 
 export class Server {
+
     constructor() {
-        onNet(Events.playerLoaded, () => {
-            const src = global.source;
-            playerManager.Add(new Player(src));
-        })
+        // Events
+        onNet(Events.requestPlayers, this.EVENT_RequestPlayers);
+        on(Events.playerJoining, this.EVENT_PlayerLoaded);
+        on(Events.playerDropped, this.EVENT_PlayerDropped);
+    }
 
-        onNet(Events.requestPlayers, () => {
-            playerManager.GetPlayers.forEach(async function(value) {
-                const foundPlayer = await playerManager.GetPlayer(value.handle);
-                if (foundPlayer) {
-                    foundPlayer.RefreshPing();
-                }
-            })
+    // Events
+    private EVENT_PlayerLoaded(): void {
+        playerManager.Add(new Player(source));
+    }
 
-            emitNet(Events.sendPlayers, source, GetConvarInt("sv_maxclients", 32), playerManager.GetPlayers);
-        })
+    private EVENT_RequestPlayers(): void {
+        for (let a = 0; a < playerManager.GetPlayers.length; a++) {
+            playerManager.GetPlayers[a].RefreshPing();
+        }
+        emitNet(Events.sendPlayers, source, GetConvarInt("sv_maxclients", 32), playerManager.GetPlayers);
+    }
+
+    private EVENT_PlayerDropped(): void {
+        playerManager.Remove(source);
     }
 }
 
