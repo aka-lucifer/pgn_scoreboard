280c4e2ab9c0d853e050977e10bbe231a0f3e01d
Xander Harrison <xander.harrison77@gmail.com>
2021-05-22 14:34:58 -0400

    Cleaned up code
    - Removed playerLoaded event and moved the event to the server.
    
    - Added players server id's appear above their head within a distance.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,38 +1,96 @@
 import { Events } from "../shared/enums/events";
+import { DrawText3D } from "./utils";
 
 export class Client {
+
+    private tickHandle: number = -1;
+
     constructor() {
         // Events
-        on(Events.resourceStart, (resourceName) => { // Handles getting players, if the server is restarted
-            if (GetCurrentResourceName() == resourceName) {
-                emitNet(Events.playerLoaded);
-            }
-        });
-
-        onNet(Events.sendPlayers, (maxPlayers, recievedPlayers: any[]) => {
-            SendNuiMessage(JSON.stringify({
-                type: "open_scoreboard",
-                data: {
-                    maxPlayers,
-                    players: recievedPlayers
-                }
-            }));
-        })
+        onNet(Events.sendPlayers, this.EVENT_SendPlayers);
 
         // Opens and Closes scoreboard
         RegisterKeyMapping("+open_scoreboard", "Opens the scoreboard", "keyboard", "L");
-        RegisterCommand("+open_scoreboard", () => { emitNet(Events.requestPlayers); }, false);
-        RegisterCommand("-open_scoreboard", () => { SendNuiMessage(JSON.stringify({ type: "close_scoreboard" })); }, false);
+        RegisterCommand("+open_scoreboard", this.OpenScoreboard.bind(this), false);
+        RegisterCommand("-open_scoreboard", this.CloseScoreboard.bind(this), false);
 
         // Scoreboard Page Up
         RegisterKeyMapping("+scoreboard_pageup", "Changes to next page of the scoreboard", "keyboard", "PAGEUP");
-        RegisterCommand("+scoreboard_pageup", () => { SendNuiMessage(JSON.stringify({ type: "change_page", data: { value: 1 } })) }, false);
+        RegisterCommand("+scoreboard_pageup", this.ScoreboardNextPage.bind(this), false);
         RegisterCommand("-scoreboard_pageup", undefined, false);
 
         // Scoreboard Page Down
         RegisterKeyMapping("+scoreboard_pagedown", "Changes to previous page of the scoreboard", "keyboard", "PAGEDOWN");
-        RegisterCommand("+scoreboard_pagedown", () => { SendNuiMessage(JSON.stringify({ type: "change_page", data: { value: -1 } })) }, false);
+        RegisterCommand("+scoreboard_pagedown", this.ScoreboardPrevPage.bind(this), false);
         RegisterCommand("-scoreboard_pagedown", undefined, false);
+    }
+
+    // Events
+    private OpenScoreboard(): void {
+        if (this.tickHandle != -1) { clearTick(this.tickHandle); }
+        emitNet(Events.requestPlayers);
+        this.tickHandle = setTick(this.TICK_DisplayId);
+    }
+
+    private CloseScoreboard(): void {
+        SendNuiMessage(JSON.stringify({
+            type: "close_scoreboard"
+        }));
+        clearTick(this.tickHandle);
+    }
+
+    private ScoreboardNextPage(): void {
+        SendNuiMessage(JSON.stringify({
+            type: "change_page",
+            data: {
+                value: 1
+            }
+        }))
+    }
+
+    private ScoreboardPrevPage(): void {
+        SendNuiMessage(JSON.stringify({
+            type: "change_page",
+            data: {
+                value: -1
+            }
+        }))
+    }
+
+    // Functions
+    private EVENT_SendPlayers(maxPlayers: number, recievedPlayers: any[]) {
+        SendNuiMessage(JSON.stringify({
+            type: "open_scoreboard",
+            data: {
+                maxPlayers,
+                players: recievedPlayers
+            }
+        }))
+    }
+
+    private TICK_DisplayId(): void {
+        const lPlayerPed = GetPlayerPed(-1);
+        const lPedPos = GetEntityCoords(lPlayerPed, true);
+        const players = GetActivePlayers() as Array<number>;
+        for (let a = 0; a < players.length; a++) {
+            const player = players[a];
+            const ped = GetPlayerPed(player);
+            const bone = GetPedBoneIndex(ped, GetHashKey("SKEL_Head"));
+            const pos = GetPedBoneCoords(ped, bone, 0.0, 0.0, 1.2);
+            const dist = GetDistanceBetweenCoords(lPedPos[0], lPedPos[1], lPedPos[2], pos[0], pos[1], pos[2], true);
+            if (dist <= 25.0) {
+                DrawText3D({
+                    x: pos[0],
+                    y: pos[1],
+                    z: pos[2]
+                }, 0.08, GetPlayerServerId(player).toString(), {
+                    r: 67,
+                    g: 160,
+                    b: 71,
+                    a: 255
+                })
+            }
+        }
     }
 }
 
